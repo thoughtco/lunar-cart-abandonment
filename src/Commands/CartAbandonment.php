c67d5ae728e9bc1cc3a0fe1935e0a1be6cc54e1a
Ryan Mitchell <ryan@thoughtcollective.com>
2023-12-14 16:59:50 +0000

    More bugs

--- src/Commands/CartAbandonment.php
+++ src/Commands/CartAbandonment.php
@@ -55,7 +55,7 @@ class CartAbandonment extends Command
                 
         Cart::query()
             ->active()
-            ->when(! in_array($channels, '*'), fn ($query) => $query->whereIn('channel_id', $channels))
+            ->when(! in_array('*', $channels), fn ($query) => $query->whereIn('channel_id', $channels))
             ->get()
             ->map(function ($cart) use ($now, $triggers) {
                 

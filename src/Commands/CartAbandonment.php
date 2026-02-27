0a9808eb477074dc338faeb222db65a8addbee55
Ryan Mitchell <ryan@thoughtcollective.com>
2026-02-27 11:30:03 +0000

    Scope query to only carts with lines we want

--- src/Commands/CartAbandonment.php
+++ src/Commands/CartAbandonment.php
@@ -52,18 +52,23 @@ class CartAbandonment extends Command
         }
         
         $now = now();
+        $maxIntervalAgo = 0;
         foreach ($triggers as $index => $trigger) {
             $triggers[$index]['interval'] *= 60;
             $triggers[$index]['interval_begin'] = $triggers[$index]['interval'] + (config('lunar.cart_abandonment.schedule_interval', 5) * 60) - 1;
+
+            if ($triggers[$index]['interval_begin'] > $maxIntervalAgo) {
+                $maxIntervalAgo = $triggers[$index]['interval_begin'];
+            }
         }
                 
         Cart::query()
             ->active()
             ->unmerged()
             ->when(! in_array('*', $channels), fn ($query) => $query->whereIn('channel_id', $channels))
+            ->whereHas('lines', fn ($query) => $query->where('updated_at', '>=', $now()->clone()->subSeconds($maxIntervalAgo)))
             ->get()
             ->map(function ($cart) use ($now, $triggers) {
-                
                 $latestCartLine = $cart->lines->sortByDesc('updated_at')->first()?->updated_at ?? now()->subDays(30);
 
                 $dateToUse = $latestCartLine->isAfter($cart->updated_at) ? $latestCartLine : $cart->updated_at;
